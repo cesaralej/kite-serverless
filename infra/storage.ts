6009b810aa87ce7e6a7ea1e37fc15d8b30940be8
Cesar Garcia <43119240+cesaralej@users.noreply.github.com>
2024-08-23 14:10:46 +0200

    [OPS] Storage

--- infra/storage.ts
+++ infra/storage.ts
@@ -1,1 +1,11 @@
-export const bucket = new sst.aws.Bucket("MyBucket");
+// Create an S3 bucket
+export const bucket = new sst.aws.Bucket("Uploads");
+
+// Create the DynamoDB table
+export const table = new sst.aws.Dynamo("Messages", {
+  fields: {
+    userId: "string",
+    messageId: "string",
+  },
+  primaryIndex: { hashKey: "userId", rangeKey: "messageId" },
+});
